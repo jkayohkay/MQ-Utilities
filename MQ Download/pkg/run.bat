9ce87ff68fb5d35de3c8e7bf9f39d3375c36fc9f
jason.kok <jason.kok@cgi.com>
2014-01-27 11:38:28 -0500

    Made the filename better and added batch/continuous.

--- MQ Download/pkg/run.bat
+++ MQ Download/pkg/run.bat
@@ -1,1 +1,3 @@
 java -cp com.ibm.mq.jar;connector.jar;mqdownload.jar;mqdownload.properties com.cgi.t360.util.MQDownload
+
+pause
--- MQ Download/src/com/cgi/t360/util/MQDownloadThread.java
+++ MQ Download/src/com/cgi/t360/util/MQDownloadThread.java
@@ -84,15 +84,25 @@ public class MQDownloadThread extends Thread {
 			qMgr = new MQQueueManager(qManager);
 
 			// Set up the options on the queue we wish to open
-			int openOptions = MQC.MQOO_INPUT_AS_Q_DEF;
-
+			int openOptions = MQC.MQOO_INPUT_AS_Q_DEF| MQC.MQOO_INQUIRE ;			
+			
+			boolean getMessage = true;
+			
 			// Now specify the queue that we wish to open and the open options
 			//System.out.println("Accessing queue: "+qName);
 			logger.info("Accessing queue: "+qName);
 			queue = qMgr.accessQueue(qName, openOptions);
-
-			while(true)
+			
+			if(queue.getCurrentDepth() == 0)
 			{
+				logger.info("Queue "+qName+" is empty.");
+				getMessage = false;
+			}
+			
+			while(getMessage)
+			{
+				queue = qMgr.accessQueue(qName, openOptions);
+				
 				MQMessage message = new MQMessage();
 				MQGetMessageOptions gmo = new MQGetMessageOptions();
 				gmo.options = MQC.MQGMO_WAIT;
@@ -104,7 +114,11 @@ public class MQDownloadThread extends Thread {
 				{
 					String messageString = message.readStringOfByteLength(message.getMessageLength());
 					logger.info("About to write the message: "+messageString);
-					File outputFile = new File(prop.getProperty("OutputDir")+System.currentTimeMillis()+message.messageSequenceNumber+".xml");
+					
+					String fileName = getFileName(messageString, message);
+					
+					File outputFile = new File(fileName);
+					
 					outputFile.createNewFile();
 					FileWriter fw = new FileWriter(outputFile.getAbsoluteFile());
 					BufferedWriter bw = new BufferedWriter(fw);
@@ -112,18 +126,28 @@ public class MQDownloadThread extends Thread {
 					bw.close();
 					fw.close();
 				}
+				
+				if(prop.getProperty("BatchOrContinuous") != null)
+				{
+					if((prop.getProperty("BatchOrContinuous").equalsIgnoreCase("batch"))
+							&& (queue.getCurrentDepth() == 0))
+					{
+						getMessage = false;
+					}
+				}
+				
+				queue.close();
+				qMgr.commit();
 			}
 
 		}
 		catch (MQException ex) {
 			logger.log(Level.SEVERE, "A WebSphere MQ Error occured : Completion Code "
-					+ ex.completionCode + " Reason Code " + ex.reasonCode);
-			ex.printStackTrace();
+					+ ex.completionCode + " Reason Code " + ex.reasonCode, ex);
 		}
 		catch (java.io.IOException ex) {
 			logger.log(Level.SEVERE, "An IOException occured whilst writing to the message buffer: "
-					+ ex);
-			ex.printStackTrace();
+					+ ex, ex);
 		}
 		finally 
 		{
@@ -134,10 +158,83 @@ public class MQDownloadThread extends Thread {
 				qMgr.close();
 				logger.info("Exitting.  Have a nice day.");
 			} catch (MQException ex) {
-				logger.log(Level.SEVERE, "A WebSphere MQ Error occurred : Completion Code " + ex.completionCode + "Reason Code " + ex.reasonCode);
-				ex.printStackTrace();
+				logger.log(Level.SEVERE, "A WebSphere MQ Error occurred : Completion Code " + ex.completionCode + "Reason Code " + ex.reasonCode, ex);
 			}
 		}
+	}
+
+	/**
+	 * 
+	 * @param messageString
+	 * @param message
+	 * @return
+	 */
+	private String getFileName(String messageString, MQMessage message) 
+	{		
+		StringBuffer fileName = new StringBuffer();
+		
+		// Start with the path to the output dir/folder
+		fileName.append(prop.getProperty("OutputDir"));
+		// Add the filesystem path separator if not already there
+		if(!fileName.toString().endsWith(File.separator))
+		{
+			fileName.append(File.separator);
+		}
+		
+		// Add the MessageID
+		if( (messageString.indexOf("<MessageID>") > 0)
+				&& (messageString.indexOf("</MessageID>") > 0) )
+		{
+			String value = messageString.substring((messageString.indexOf("<MessageID>")+"<MessageID>".length()),
+											(messageString.indexOf("</MessageID>")));
+			fileName.append("MsgID_"+value);
+		}
+		
+		// Add the DestinationID
+		if( (messageString.indexOf("<DestinationID>") > 0)
+				&& (messageString.indexOf("</DestinationID>") > 0) )
+		{
+			String value = messageString.substring((messageString.indexOf("<DestinationID>")+"<DestinationID>".length()),
+											(messageString.indexOf("</DestinationID>")));
+			fileName.append(".DestID_"+value);
+		}
+		
+		// Add the SenderID
+		if( (messageString.indexOf("<SenderID>") > 0)
+				&& (messageString.indexOf("</SenderID>") > 0) )
+		{
+			String value = messageString.substring((messageString.indexOf("<SenderID>")+"<SenderID>".length()),
+											(messageString.indexOf("</SenderID>")));
+			fileName.append(".SenderID_"+value);
+		}
+		
+		// Add the MessageType
+		if( (messageString.indexOf("<MessageType>") > 0)
+				&& (messageString.indexOf("</MessageType>") > 0) )
+		{
+			String value = messageString.substring((messageString.indexOf("<MessageType>")+"<MessageType>".length()),
+											(messageString.indexOf("</MessageType>")));
+			fileName.append(".MsgType_"+value);
+		}
+		
+		// Add the current time in milliseconds and message sequence number to make 
+		// the filename unique
+		if(fileName.toString().endsWith(File.separator))
+		{
+			fileName.append(System.currentTimeMillis()
+					+message.messageSequenceNumber);
+		}
+		else
+		{
+			fileName.append("."
+					+System.currentTimeMillis()
+					+message.messageSequenceNumber);
+		}
+		
+		// Add the file extension
+		fileName.append(".xml");
+		
+		return fileName.toString();
 	}
 	
 }
