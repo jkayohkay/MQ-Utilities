3b9c34534dfee8f4b0e6193f9d09c7c82c6b7e2b
jason.kok <jason.kok@cgi.com>
2014-01-29 10:37:33 -0500

    Added JAVA_HOME to bat file.

--- MQ Download/pkg/run.bat
+++ MQ Download/pkg/run.bat
@@ -1,3 +1,5 @@
-java -cp com.ibm.mq.jar;connector.jar;mqdownload.jar;mqdownload.properties com.cgi.t360.util.MQDownload
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_45
+
+"%JAVA_HOME%\bin\java" -cp com.ibm.mq.jar;connector.jar;mqdownload.jar;mqdownload.properties com.cgi.t360.util.MQDownload
 
 pause
